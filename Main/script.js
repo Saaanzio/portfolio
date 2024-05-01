f294d4f8d5e43104754c2f0d8617a5e95adf0827
Saaanzio <rafaelsanziosilva22@gmail.com>
2024-04-30 22:14:58 -0300

    Criação da segunda parte do site

--- Main/script.js
+++ Main/script.js
@@ -1,11 +1,21 @@
+//Tive ajuda nessa função :^)
 const scrollArrow = document.getElementById('seta');
 window.addEventListener('scroll', function() {
     const scrollY = window.scrollY || window.pageYOffset;
-    const hidePoint = window.innerHeight / 2;
-
+    const documentHeight = document.body.scrollHeight - window.innerHeight;
+    const hidePoint = documentHeight * 0.8;
     if (scrollY > hidePoint) {
         scrollArrow.style.opacity = '0';
     } else {
-        scrollArrow.style.opacity = '1';
+        const opacity = 1 - (scrollY / hidePoint);
+        scrollArrow.style.opacity = opacity.toFixed(2);
     }
 });
+function smoothScrollDown() {
+    window.scrollBy({
+        top: 1200, 
+        behavior: 'smooth' 
+    });
+}
+
+scrollArrow.addEventListener('click', smoothScrollDown);
